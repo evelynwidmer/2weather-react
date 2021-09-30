5399fd976abdc50dba3317cf2f7e5cb4739b6b7e
evelynwidmer <evelyn4720@gmail.com>
2021-09-29 22:51:14 -0500

    Added URL for Github repository at the bottom of the page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <footer>
+        This project was coded by Evelyn Widmer and is{" "}
+        <a
+          href="https://github.com/evelynwidmer/2weather-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
